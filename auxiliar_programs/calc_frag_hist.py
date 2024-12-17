f07d271ac17e13c13321f6752fd7451e1c3330d7
Paulo Cordeiro <paulocordeiro@gmail.com>
2024-12-17 09:27:01 -0500

    included samples

--- auxiliar_programs/calc_frag_hist.py
+++ auxiliar_programs/calc_frag_hist.py
@@ -65,9 +65,15 @@ dt_frag_list.sort()
 for i in list(range(len(dt_frag_list))):
     # Create dataframe from csv file for each file and save to a list of data frames
     dt_frag_i = pd.read_csv(dt_frag_list[i], sep="\t", names=["Size", "Occurrences","Sample"],skiprows=2)
-    #df = pd.read_csv("NEPCmlf010.fragment_sizes.txt", sep="\t", skiprows=1, names=["Size", "Occurrences", "Sample"])
     dt_frag_i = dt_frag_i.drop('Sample', axis=1)
     frag_base_i = os.path.basename(dt_frag_list[i])
+
+    # Split txt file paths on dots
+    sample_id_list = frag_base_i.split(".")
+
+    # Join list on the elements of the sample id
+    separator = ""
+    sample_id = separator.join(sample_id_list[0:-2])
 
     # Round column and convert occurrences to int
     dt_frag_i = dt_frag_i.round(1)
@@ -75,14 +81,21 @@ for i in list(range(len(dt_frag_list))):
 
     # Create long forms of fragment histograms
     dt_frag_i_long = np.repeat(dt_frag_i["Size"].values, dt_frag_i["Occurrences"].values)
+    dt_sample_i_long = np.repeat(sample_id, len(dt_frag_i_long))
+    dt_sample_i_short = np.repeat(sample_id, dt_frag_i.shape[0])
 
     if i == 0:
         frags_arr = dt_frag_i_long
+        sample_arr = dt_sample_i_long
+        sample_short = dt_sample_i_short
         frag_hist = dt_frag_i
     else:
         frags_arr = np.append(frags_arr, dt_frag_i_long)
+        sample_arr = np.append(sample_arr, dt_sample_i_long)
+        sample_short = np.append(sample_short, dt_sample_i_short)
         frag_hist = frag_hist.append(dt_frag_i)
 
+frag_hist["sample"] = sample_short
 frag_hist = frag_hist.reset_index(drop=True)
 
 size_list = frag_hist["Size"].to_numpy().astype(str)
@@ -90,11 +103,20 @@ occurrences_list = frag_hist["Occurrences"].to_numpy().astype(str)
 size_list_sep = np.core.defchararray.add(size_list, " : ")
 x_y_list = np.core.defchararray.add(size_list_sep, occurrences_list)
 
+sample_grp = frag_hist[["sample"]].groupby(["sample"]).size().reset_index()
 first_line = "data:"
 
-x_y_str = ", ".join(x_y_list)
-full_line_i = f"    'fragment_histogram' : {{{x_y_str}}}"
-frag_len_hist_mqc_dict = "\n".join([first_line, full_line_i])
+for i in list(range(sample_grp.shape[0])):
+    sample_i = sample_grp.at[i, "sample"]
+    str_list = x_y_list[(frag_hist["sample"] == sample_i)]
+
+    x_y_str = ", ".join(str_list)
+    full_line_i = "    '" + sample_i + "' : {" + x_y_str + "}"
+    if i == 0:
+        frag_len_hist_mqc_dict = "\n".join([first_line, full_line_i])
+
+    else:
+        frag_len_hist_mqc_dict = "\n".join([frag_len_hist_mqc_dict, full_line_i])
 
 txt_mqc = open(args.output, "w")
 txt_mqc.write(frag_len_hist_mqc_dict)
