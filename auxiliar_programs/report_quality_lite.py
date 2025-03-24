326ec0baf3f013c18a2311e7385f6ac5c8377fb7
Paulo Cordeiro <paulocordeiro@gmail.com>
2025-03-24 19:40:17 -0400

    Single End ready

--- auxiliar_programs/report_quality_lite.py
+++ auxiliar_programs/report_quality_lite.py
@@ -3,11 +3,12 @@
 import os
 import pandas as pd
 
-
 def load_csv_from_current_directory(filename):
     current_files = os.listdir(os.getcwd())
     if filename in current_files:
         return pd.read_csv(filename, comment='#')
+    else:
+        return pd.DataFrame()
 
 str_frags = 'frags_mqc.csv'
 str_peaks = 'peaks_mqc.csv'
@@ -22,33 +23,32 @@ def load_enrichment_csvs():
         f for f in current_files
         if f.startswith('enrichment') and f.endswith('.csv')]
     
-    dataframes = [pd.read_csv(f) for f in enrichment_files]
-    dataframes_concat = pd.concat(dataframes, ignore_index=True)
-    
-    return dataframes_concat
+    if enrichment_files:
+        dataframes = [pd.read_csv(f) for f in enrichment_files]
+        return pd.concat(dataframes, ignore_index=True)
+    else:
+        return pd.DataFrame()  # return empty if no enrichment files
 
 df_enrichment = load_enrichment_csvs()
 
-def join_sample_dataframes(df_frags,df_peaks,df_enrichment):
+def join_sample_dataframes(df_frags, df_peaks, df_enrichment):
     merged = pd.merge(df_frags, df_peaks, on='SampleName', how='inner')
-    merged = pd.merge(merged, df_enrichment, on='SampleName', how='outer')
+    if not df_enrichment.empty:
+        merged = pd.merge(merged, df_enrichment, on='SampleName', how='outer')
     return merged
 
-dfJoin = join_sample_dataframes(df_frags,df_peaks,df_enrichment)
-dfJoin = dfJoin.drop(columns=['on_bp', 'off_bp','on_reads','off_reads'], errors='ignore')
+dfJoin = join_sample_dataframes(df_frags, df_peaks, df_enrichment)
+dfJoin = dfJoin.drop(columns=['on_bp', 'off_bp', 'on_reads', 'off_reads'], errors='ignore')
 
 dfJoin = dfJoin.rename(columns={
-            'SampleName': 'Sample',
-            'Fragments': 'TotalFragments',
-            'Peaks': 'TotalPeaks',
-            'mark':'Enrichment_Mark',
-            'enrichment':'Enrichment_Score'})
+    'SampleName': 'Sample',
+    'Fragments': 'TotalFragments',
+    'Peaks': 'TotalPeaks',
+    'mark': 'Enrichment_Mark',
+    'enrichment': 'Enrichment_Score'
+})
 
-dfJoin = dfJoin.where(pd.notnull(dfJoin),'')
+dfJoin = dfJoin.where(pd.notnull(dfJoin), '')
 
 filename = 'QualityMetrics.csv'
-
 dfJoin.to_csv(filename, index=False, encoding='utf-8')
-
-
-
