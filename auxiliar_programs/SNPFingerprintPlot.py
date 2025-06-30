9e1f06e932052c36521e59ac64fa3e881e44391b
Paulo Cordeiro <paulocordeiro@gmail.com>
2025-06-30 14:34:20 -0300

    size fingerprintSMaSH dynamic

--- auxiliar_programs/SNPFingerprintPlot.py
+++ auxiliar_programs/SNPFingerprintPlot.py
@@ -50,7 +50,7 @@ def plot_pvalue_heatmap_sorted_dend(df_original, output_path="heatmap.jpg"):
     
     row_linkage = linkage(row_dist, method='average')
     col_linkage = linkage(col_dist, method='average')
-    size = max(df_original.shape[0]/2,8)
+    size = max(df_original.shape[0]/2,10)
     figsize=(size, size)
     
     g = sns.clustermap(df_original, cmap="coolwarm", linewidths=0.5, annot=False, fmt=".1e",
